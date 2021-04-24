5f9fccc77802da02b631171368e3ae29ffd4abea
hamada <hamadagh777@gmail.com>
2021-04-24 21:22:33 +0200

    adding drop-in login user interface

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,4 @@
+import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
 import { ReactiveDict } from "meteor/reactive-dict";
 
@@ -38,6 +39,8 @@ Template.body.events({
     Tasks.insert({
       text,
       createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
     });
 
     target.text.value = "";
