10248c9002dbd4fb5402878c8e69c284c154a336
hamada <hamadagh777@gmail.com>
2021-04-24 21:15:05 +0200

    adding checkbox to hide completed task

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,30 @@
 import { Template } from "meteor/templating";
+import { ReactiveDict } from "meteor/reactive-dict";
 
 import { Tasks } from "../api/tasks";
 
 import "./task.js";
 import "./body.html";
 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
   tasks() {
+    const instance = Template.instance();
+    if (instance.state.get("hideCompleted")) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find(
+        { checked: { $ne: true } },
+        { sort: { createdAt: -1 } }
+      );
+    }
+    // Otherwise, return all of the tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
 
@@ -24,5 +41,8 @@ Template.body.events({
     });
 
     target.text.value = "";
+  },
+  "change .hide-completed input"(event, instance) {
+    instance.state.set("hideCompleted", event.target.checked);
   },
 });
