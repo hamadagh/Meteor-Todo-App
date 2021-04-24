ec3774222cc61d9691ff07f156a509c94deaf2fc
hamada <hamadagh777@gmail.com>
2021-04-24 09:50:57 +0200

    adding input field in body.html and event handler in body.js

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -9,3 +9,19 @@ Template.body.helpers({
     return Tasks.find({});
   },
 });
+
+Template.body.events({
+  "submit .new-task"(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    target.text.value = "";
+  },
+});
